5d67a96a200d60372f792fd0fd12af7ac0685531
venkey <venkeyvenkatesh885@gmail.com>
2020-10-21 11:54:03 +0530

    first commit

---
+++ LinkedList/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LinkedList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the LinkedList Demo Program");
+        }
+    }
+}
